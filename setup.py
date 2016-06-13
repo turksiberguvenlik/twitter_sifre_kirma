a2a9c66b33dd7513e618e78d54e2f59c4af15d5f
Cameron Poe <cameron@zay.li>
2016-06-13 07:53:25 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+    name = "Eros",
+    py_modules = ["eros"],
+    version = "0.0.1",
+    description = "Twitter Password Cracker in Python.",
+    author = "Cameron Poe",
+    author_email = "cameron@zay.li",
+    url = "https://github.com/x9zr3/eros",
+    keywords = ["hacking", "password", "cracking"]
+)
